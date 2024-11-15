9afd67befeb955a2f1b8359f9b6479e3e6fe3174
liuhanzuo <lhz24@mails.tsinghua.edu.cn>
2024-11-15 22:31:06 +0800

    5

--- part3/eval.sh
+++ part3/eval.sh
@@ -1,13 +1,13 @@
 echo "model_type: $1"
 echo "dataset_dir: ./data/rnn/cot_binary_32_1000000_right"
-echo "output_dir: ./output_$1"
-echo "model_dir: ./output_$1_$2/$3"
+echo "output_dir: ./output_$1_$2_$4"
+echo "model_dir: ./output_$1_$2_$4/$3"
 echo "model_config_path: ./configs/$2m_transformer.json"
 echo "batch_size: 1"
 python3 rnn/val.py \
     --model_type $1 \
     --dataset_dir ./data/rnn/cot_binary_32_1000000_right  \
-    --output_dir ./output_$1_$2 \
-    --model_dir ./output_$1_$2/$3\
+    --output_dir ./output_$1_$2_$4 \
+    --model_dir ./output_$1_$2_$4/$3\
     --model_config_path ./configs/$2m_transformer.json \
     --batch_size 1
