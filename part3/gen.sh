05b98edfff652ea4f217f71f7ec0fc274d6105fc
liuhanzuo <lhz24@mails.tsinghua.edu.cn>
2024-11-15 16:32:40 +0800

    2

---
+++ part3/gen.sh
@@ -0,0 +1,5 @@
+python rnn/data.py \
+    --n_nodes 32 \
+    --graph_type binary \
+    --task_type cot \
+    --size 1000000
--- part3/train.sh
+++ part3/train.sh
@@ -1,6 +1,6 @@
 echo "model_type: $1"
 echo "dataset_dir: ./data/rnn/cot_binary_32_1000000_right"
-echo "output_dir: ./output_$1_$2"
+echo "output_dir: ./output_$1_$2_$3"
 echo "model_config_path: ./configs/$2m_transformer.json"
 echo "batch_size: 64"
 echo "total_training_samples: $3"
@@ -8,7 +8,7 @@ echo "lr: $4"
 python3 rnn/train.py \
     --model_type $1 \
     --dataset_dir ./data/rnn/cot_binary_32_1000000_right \
-    --output_dir ./output_$1_$2 \
+    --output_dir ./output_$1_$2_$3 \
     --model_config_path ./configs/$2m_transformer.json \
     --batch_size 64 \
     --total_training_samples $3 \
